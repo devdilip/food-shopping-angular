c4ac997b964ac97d03675a5f234378ce87628c4c
Dilip Kumar <dilip.kumar@mountblue.io>
2018-09-18 16:16:08 +0530

    initial commit

---
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+
+import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
+
+@Component({
+  selector: 'app-recipe-detail',
+  templateUrl: './recipe-detail.component.html',
+  styleUrls: ['./recipe-detail.component.css']
+})
+export class RecipeDetailComponent implements OnInit {
+  recipe: Recipe;
+  id: number;
+
+  constructor(private recipeService: RecipeService, 
+              private route: ActivatedRoute, 
+              private router:Router) { }
+
+  onAddToShhopingList(){
+    this.recipeService.addIngredientsToShhopingList(this.recipe.ingredients);
+  }
+
+  ngOnInit() { 
+    // const id = this.route.snapshot.params['id']; 
+    this.route.params.subscribe(
+      (params: Params) => {
+        this.id = +params['id'];
+        this.recipe = this.recipeService.getRecipe(this.id);
+      }
+    );
+  }
+
+  onEditRecipe(){
+    this.router.navigate(['edit'], {relativeTo: this.route});
+    // this.router.navigate(['../', this.id, 'edit'], {relativeTo: this.route});
+  }
+
+  onDeleteRecipe(){
+    this.recipeService.deleteRecipe(this.id);
+    this.router.navigate(['/recipes']);
+  }
+
+}
